b43355976b13ffd635cc2f7a3e62931c1ccd8937
Emilian Roman <roman.emilian@outlook.com>
2018-09-14 23:04:05 +0800

    Implement dynamic banner

--- Miris.Bbkpify.CLI/Program.cs
+++ Miris.Bbkpify.CLI/Program.cs
@@ -1,6 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
+using System.Text;
+using static System.AppDomain;
 using static System.Console;
+using static System.ConsoleColor;
 using static System.Environment;
 using static System.IO.File;
 
@@ -17,6 +21,8 @@ namespace Miris.Bbkpify.CLI
 
         public static void Main(string[] args)
         {
+            ShowBanner();
+            
             ExitIfFalse(args.Length >= 3, "Not enough arguments provided.", 1);
 
             var placeholderPath = args[0];
@@ -51,6 +57,40 @@ namespace Miris.Bbkpify.CLI
             if (condition) return;
             WriteLine(exitMessage);
             Exit(exitCode);
+        }
+
+        private static void ShowBanner()
+        {
+            ForegroundColor = Magenta;
+            WriteLine(@"
+    __    __    __         _ ____     
+   / /_  / /_  / /______  (_) __/_  __
+  / __ \/ __ \/ //_/ __ \/ / /_/ / / /
+ / /_/ / /_/ / ,< / /_/ / / __/ /_/ / 
+/_.___/_.___/_/|_/ .___/_/_/  \__, /  
+                /_/          /____/   
+======================================
+");
+            var availableTypes = new Func<string>(() =>
+            {
+                var x = new StringBuilder();
+
+                for (var i = 0; i < Types.Length; i++)
+                {
+                    var s = i + 1 == Types.Length ? string.Empty : " | ";
+                    x.Append($"'{Types[i]}'{s}");
+                }
+
+                return x.ToString();
+            })();
+            
+            ForegroundColor = Cyan;
+            WriteLine($@"
+Usage: .\{CurrentDomain.FriendlyName} <1> <2> <3>
+         1 - Placeholder file path (e.g. '.\placeholder.bmp', 'C:\placeholder.bmp')
+         2 - Files directory path (e.g. '.\cmt\tags', 'C:\cmt\tags')
+         3 - One of the following: {availableTypes}
+");
         }
     }
 }

3594d5d2743e665b495bd7a8e807e6a7b4158705
Emilian Roman <roman.emilian@outlook.com>
2018-09-15 07:25:07 +0800

    Implement Unbkkpify

---
+++ YuMi.Unbbkpify.CLI/Program.Banner.cs
@@ -0,0 +1,21 @@
+﻿using YuMi.Bbkpify;
+using static System.AppDomain;
+using static System.Console;
+using static System.ConsoleColor;
+
+namespace YuMi.Unbbkpify.CLI
+{
+    internal static partial class Program
+    {
+        private static void ShowBanner()
+        {
+            ForegroundColor = Magenta;
+            WriteLine(Ascii.Banner);
+            ForegroundColor = Cyan;
+            WriteLine($@"
+Usage: .\{CurrentDomain.FriendlyName} <1>
+         1 - Directory name to undo the bbkpify process in (e.g. '.\cmt\tags', 'C:\cmt\tags')
+");
+        }
+    }
+}
---
+++ YuMi.Unbbkpify.CLI/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using YuMi.Bbkpify;
+using static System.Console;
+using static System.ConsoleColor;
+using static System.Environment;
+using static YuMi.Bbkpify.Core;
+using static YuMi.Bbkpify.ExitCodes;
+
+namespace YuMi.Unbbkpify.CLI
+{
+    internal static partial class Program
+    {
+        internal static void Main(string[] args)
+        {
+            ShowBanner();
+
+            var directoryPath = string.Empty;
+
+            if (args.Length == 0)
+            {
+                ForegroundColor = Cyan;
+                while (!Directory.Exists(directoryPath))
+                {
+                    WriteLine("Please input a valid directory path:");
+                    directoryPath = ReadLine();
+                    ForegroundColor = Red;
+                }
+            }
+            else
+            {
+                directoryPath = args[0];
+
+                if (!Directory.Exists(directoryPath))
+                {
+                    ForegroundColor = Red;
+                    WriteLine("Target folder does not exist.");
+                    Exit((int)InvalidFilesFolderPath);
+                }
+            }
+            
+            var files = Directory.GetFiles(directoryPath, $"*.{Extension}*");
+            UnbkkpifyFiles(files);
+            
+            WriteLine($"\nFinished restoring bitmaps in '{directoryPath}'!");
+            Exit((int)Success);
+        }
+
+        private static void UnbkkpifyFiles(string[] files)
+        {
+            ForegroundColor = Green;
+            for (var i = 0; i < files.Length; i++)
+            {
+                var currentFile = files[i];
+                var placeholder = currentFile.Substring(0, currentFile.Length - Extension.Length - 1);
+
+                var progress = Ascii.Progress(i, files.Length);
+                WriteLine($"{progress}\t| RESTORING {placeholder}");
+                
+                File.Delete(placeholder);
+                File.Move(currentFile, placeholder);
+            }
+        }
+    }
+}

e0de0c4add2de9ae740ccb002205bb7a79a7b452
Emilian Roman <roman.emilian@outlook.com>
2018-09-15 08:01:37 +0800

    Add msbuild command, use Git versioning

--- Compile.sh
+++ Compile.sh
@@ -5,12 +5,15 @@
 BIN_DIR="YuMi.Bbkpify.Build"
 ISO_DIR="YuMi.Bbkpify.ISO"
 
+# Building
+msbuild /t:Build /p:Configuration=Release /p:TargetFramework=v4.5
+
 # Preparation
 mkdir -p "${BIN_DIR}" "${ISO_DIR}"
 rsync -rav --progress ./*/bin/Release/*.{dll,exe} README.md "${BIN_DIR}"
 
 # Compiling
-ISO_NAME="YuMi.Bbkpify.CLI.$(date +%s)"
+ISO_NAME="YuMi.Bbkpify.CLI.$(git describe --tags $(git rev-list --tags --max-count=1))"
 ISO_PATH="${ISO_DIR}/${ISO_NAME}.iso" && mkisofs -v -udf -iso-level 4 -V "${ISO_NAME}" -o "${ISO_PATH}" "${BIN_DIR}"
 
 # Signing

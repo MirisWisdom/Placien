b3808bcb83c59da9ccb30cbb9cc4d82afb5a1350
Emilian Roman <roman.emilian@outlook.com>
2019-02-16 19:08:57 +0800

    Define licence header and details in the core assembly information

--- src/SPV3.Bbkpify.Core/Properties/AssemblyInfo.cs
+++ src/SPV3.Bbkpify.Core/Properties/AssemblyInfo.cs
@@ -1,15 +1,34 @@
-﻿using System.Reflection;
+﻿/**
+ * Copyright (C) 2018-2019 Emilian Roman
+ * 
+ * This file is part of SPV3.Bbkpify.
+ * 
+ * SPV3.Bbkpify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SPV3.Bbkpify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SPV3.Bbkpify.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SPV3.Bbkpify.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Core library for SPV3.Bbkpify")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Yu ~ Miris")]
 [assembly: AssemblyProduct("SPV3.Bbkpify.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2018-2019 Emilian Roman")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

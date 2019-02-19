672e970dbc8eefdb44736d779781a3ac6ccf8b6a
Emilian Roman <roman.emilian@outlook.com>
2019-02-19 19:51:39 +0800

    Introduce the GUI assembly

---
+++ src/SPV3.Bbkpify.GUI/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace SPV3.Bbkpify.GUI
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}

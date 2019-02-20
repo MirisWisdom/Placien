46c7a6dc05b23061dd8d41e28ea68ca5082b0fff
Emilian Roman <roman.emilian@outlook.com>
2019-02-20 20:30:07 +0800

    Update the namespaces, licence and formatting
    Squashed commit of the following:
    
    commit e95dc88a7df5cd2053ac85f4424fbf55f80bd67f
    Author: Emilian Roman <roman.emilian@outlook.com>
    Date:   Wed Feb 20 20:29:34 2019 +0800
    
        Format the src files
    
    commit 81a7f46ccfb635a1bc75b511b7e48ab80e9887d5
    Author: Emilian Roman <roman.emilian@outlook.com>
    Date:   Wed Feb 20 20:29:24 2019 +0800
    
        Define formatting rules
    
    commit 929b3f82065684868b10b88caf8328488f2b2130
    Author: Emilian Roman <roman.emilian@outlook.com>
    Date:   Wed Feb 20 20:25:38 2019 +0800
    
        Change assembly & namespace prefix to SPV3
    
    commit 2d7d0e190af0d611ef6e652f7d34c7e6c59174d0
    Author: Emilian Roman <roman.emilian@outlook.com>
    Date:   Wed Feb 20 20:05:53 2019 +0800
    
        Move the solution Git ignore to the src directory
    
    commit 1ffb43bf97550c6e206ffae45c3b4b37c75fa685
    Author: Emilian Roman <roman.emilian@outlook.com>
    Date:   Wed Feb 20 20:05:20 2019 +0800
    
        Add licence headers to all CS files
    
    commit dcb8c5e7009fd883808b17e0da84635f4922f536
    Author: Emilian Roman <roman.emilian@outlook.com>
    Date:   Wed Feb 20 20:04:03 2019 +0800
    
        Change licence to GPLv3
    
    commit f5039a007a1bef111753eb83ab597439611bed80
    Author: Emilian Roman <roman.emilian@outlook.com>
    Date:   Wed Feb 20 20:02:53 2019 +0800
    
        Move source code to the src directory

---
+++ src/SPV3.Bbkpify/Ascii.cs
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2018-2019 Emilian Roman
+ * 
+ * This file is part of SPV3.Bbkpify.
+ * 
+ * SPV3.Bbkpify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SPV3.Bbkpify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SPV3.Bbkpify.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace SPV3.Bbkpify
+{
+  public static class Ascii
+  {
+    public static string Banner => @"
+    __    __    __         _ ____     
+   / /_  / /_  / /______  (_) __/_  __
+  / __ \/ __ \/ //_/ __ \/ / /_/ / / /
+ / /_/ / /_/ / ,< / /_/ / / __/ /_/ / 
+/_.___/_.___/_/|_/ .___/_/_/  \__, /  
+                /_/          /____/   
+======================================
+                           // Yu:YuMi
+";
+
+    public static string Progress(int index, int arrayLength)
+    {
+      return $"[{index + 1}/{arrayLength}]";
+    }
+  }
+}
---
+++ src/SPV3.Unbbkpify.CLI/Program.cs
@@ -0,0 +1,97 @@
+/**
+ * Copyright (C) 2018-2019 Emilian Roman
+ * 
+ * This file is part of SPV3.Bbkpify.
+ * 
+ * SPV3.Bbkpify is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SPV3.Bbkpify is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SPV3.Bbkpify.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.IO;
+using SPV3.Bbkpify;
+using YuMi.Output;
+
+namespace SPV3.Unbbkpify.CLI
+{
+  /// <summary>
+  ///     Main program class.
+  /// </summary>
+  internal static class Program
+  {
+    /// <summary>
+    ///     Console program entry.
+    /// </summary>
+    /// <param name="args">
+    ///     args[0]: BBKP bitmaps directory path
+    /// </param>
+    internal static void Main(string[] args)
+    {
+      ShowBanner();
+
+      var directoryPath = string.Empty;
+
+      if (args.Length < 1)
+      {
+        Line.Write("Not enough arguments provided. Using manual input...", ConsoleColor.Yellow, "WARN");
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        while (DirectoryValidator.GetStatus(directoryPath) != DirectoryStatus.IsValid)
+        {
+          Line.Write("Please input a valid directory path:", ConsoleColor.Cyan, "STEP");
+          directoryPath = Console.ReadLine();
+        }
+      }
+      else
+      {
+        directoryPath = args[0];
+
+        if (DirectoryValidator.GetStatus(directoryPath) == DirectoryStatus.DoesNotExist)
+        {
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.Error.WriteLine("Target folder does not exist.");
+          Environment.Exit((int) ExitCodes.InvalidFilesFolderPath);
+        }
+      }
+
+      var files = Directory.GetFiles(directoryPath, $"*.{Bbkpify.Main.Extension}*", SearchOption.AllDirectories);
+
+      try
+      {
+        Bbkpify.Main.ResetBitmapFiles(files);
+      }
+      catch (Exception e)
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Error.WriteLine(e.Message);
+        Environment.Exit((int) ExitCodes.ExceptionHasBeenThrown);
+      }
+
+      Line.Write($"\nRestored bitmaps in '{directoryPath}'!", ConsoleColor.Green, "DONE");
+      Console.ReadLine();
+      Environment.Exit((int) ExitCodes.Success);
+    }
+
+    /// <summary>
+    ///     Outputs the main ASCII banner.
+    /// </summary>
+    private static void ShowBanner()
+    {
+      Line.Write(Ascii.Banner, ConsoleColor.Magenta);
+      Line.Write($@"
+Usage: .\{AppDomain.CurrentDomain.FriendlyName} <1>
+         1 - Directory name to undo the bbkpify process in (e.g. '.\cmt\tags', 'C:\cmt\tags')
+", ConsoleColor.Cyan);
+    }
+  }
+}
